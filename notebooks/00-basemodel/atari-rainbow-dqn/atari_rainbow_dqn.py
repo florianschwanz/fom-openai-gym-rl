c9b5d3f1918c2a9f8adac0cd2c527495c83622bd
Florian Schwanz <florian.schwanz@interoberlin.de>
2020-06-13 19:58:00 +0200

    Track episode rewards

--- notebooks/00-basemodel/atari-rainbow-dqn/atari_rainbow_dqn.py
+++ notebooks/00-basemodel/atari-rainbow-dqn/atari_rainbow_dqn.py
@@ -1,10 +1,12 @@
 import os
 import sys
+import time
+from datetime import datetime
 
-import numpy as np
 import torch
 import torch.autograd as autograd
 import torch.optim as optim
+from tqdm import tqdm
 
 # Make library available in path
 lib_path = os.path.join(os.getcwd(), 'lib')
@@ -15,9 +17,15 @@ if not (lib_path in sys.path):
 from deep_q_network import RainbowCnnDQN
 from environment_enum import Environment
 from model_optimizer import ModelOptimizer
+from performance_logger import PerformanceLogger
 from replay_buffer import ReplayBuffer
-from tqdm import tqdm
 from wrappers import make_atari, wrap_deepmind, wrap_pytorch
+
+RUN_DIRECTORY = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+
+# Only use defined parameters if there is no previous model being loaded
+FINISHED_FRAMES = 0
+FINISHED_EPISODES = 0
 
 # Define setup
 ENVIRONMENT_NAME = Environment.PONG_NO_FRAMESKIP_v4
@@ -26,7 +34,7 @@ GAMMA = 0.99
 NUM_ATOMS = 51
 VMIN = -10
 VMAX = 10
-REPLAY_INITIAL = 20_000
+TARGET_UPDATE = 1_000
 REPLAY_MEMORY_SIZE = 100_000
 NUM_FRAMES = 1_000_000
 
@@ -41,8 +49,9 @@ Variable = lambda *args, **kwargs: autograd.Variable(*args, **kwargs).cuda(True)
 def update_target(current_model, target_model):
     target_model.load_state_dict(current_model.state_dict())
 
+
 # Initialize environment
-env = make_atari(ENVIRONMENT_NAME)
+env = make_atari(ENVIRONMENT_NAME.value)
 env = wrap_deepmind(env, frame_stack=True, scale=False)
 env = wrap_pytorch(env)
 
@@ -56,9 +65,18 @@ update_target(policy_net, target_net)
 # Initialize replay memory
 memory = ReplayBuffer(REPLAY_MEMORY_SIZE)
 
-losses = []
-all_rewards = []
-episode_reward = 0
+# Initialize total variables
+total_frames = 0
+total_episodes = FINISHED_EPISODES
+total_original_rewards = []
+total_shaped_rewards = []
+total_start_time = time.time()
+
+# Initialize episode variables
+episode_frames = 0
+episode_original_reward = 0
+episode_shaped_reward = 0
+episode_start_time = time.time()
 
 # Initialize the environment and state
 state = env.reset()
@@ -72,36 +90,75 @@ for total_frames in progress_bar:
     # Perform action
     next_state, reward, done, _ = env.step(action)
 
+    # Shape reward
+    original_reward = reward
+    shaped_reward = reward
+
+    # Add reward to episode reward
+    episode_original_reward += original_reward
+    episode_shaped_reward += shaped_reward
+
     # Store the transition in memory
     memory.push(state, action, reward, next_state, done)
 
     # Move to the next state
     state = next_state
-    episode_reward += reward
-
-    if done:
-        state = env.reset()
-        all_rewards.append(episode_reward)
-        episode_reward = 0
 
     # Perform one step of the optimization (on the target network)
-    if len(memory) > REPLAY_INITIAL:
-        loss = ModelOptimizer.compute_td_loss(policy_net=policy_net,
-                                              target_net=target_net,
-                                              optimizer=optimizer,
-                                              memory=memory,
-                                              batch_size=BATCH_SIZE,
-                                              num_atoms=NUM_ATOMS,
-                                              vmin=VMIN,
-                                              vmax=VMAX,
-                                              USE_CUDA=USE_CUDA)
-        losses.append(loss.item())
+    loss = ModelOptimizer.compute_td_loss(policy_net=policy_net,
+                                          target_net=target_net,
+                                          optimizer=optimizer,
+                                          memory=memory,
+                                          batch_size=BATCH_SIZE,
+                                          num_atoms=NUM_ATOMS,
+                                          vmin=VMIN,
+                                          vmax=VMAX,
+                                          USE_CUDA=USE_CUDA)
 
-    if total_frames % 3000 == 0:
-        avg_return = np.mean(all_rewards[-50:])
-        print("Average return (last 50 episodes): {:.2f}".format(avg_return))
+    if done:
+        # Track episode time
+        episode_end_time = time.time()
+        episode_duration = episode_end_time - episode_start_time
+        total_duration = episode_end_time - total_start_time
 
-    if total_frames % 1000 == 0:
-        update_target(policy_net, target_net)
+        # Add rewards to total reward
+        total_original_rewards.append(episode_original_reward)
+        total_shaped_rewards.append(episode_shaped_reward)
 
-policy_net.act()
+        if loss is not None:
+            PerformanceLogger.log_episode(directory=RUN_DIRECTORY,
+                                          total_episodes=total_episodes + 1,
+                                          total_frames=total_frames,
+                                          total_duration=total_duration,
+                                          total_original_rewards=total_original_rewards,
+                                          total_shaped_rewards=total_shaped_rewards,
+                                          episode_frames=episode_frames + 1,
+                                          episode_original_reward=episode_original_reward,
+                                          episode_shaped_reward=episode_shaped_reward,
+                                          episode_loss=loss.item(),
+                                          episode_duration=episode_duration)
+
+        # Update the target network, copying all weights and biases from policy net into target net
+        if total_episodes % TARGET_UPDATE == 0:
+            target_net.load_state_dict(policy_net.state_dict())
+
+        # Reset episode variables
+        episode_frames = 0
+        episode_original_reward = 0
+        episode_shaped_reward = 0
+        episode_start_time = time.time()
+
+        # Reset the environment and state
+        state = env.reset()
+
+        # Increment counter
+        total_episodes += 1
+
+    # Increment counter
+    episode_frames += 1
+
+print('Complete')
+# env.render()
+# env.close()
+# plt.ioff()
+# plt.show()
---
+++ notebooks/00-basemodel/atari-rainbow-dqn/lib/performance_logger.py
@@ -0,0 +1,39 @@
+import os
+
+import numpy as np
+
+
+class PerformanceLogger:
+
+    def log_episode(directory, total_episodes, total_frames, total_duration, total_original_rewards,
+                    total_shaped_rewards, episode_frames, episode_original_reward,
+                    episode_shaped_reward, episode_loss, episode_duration):
+        path = "./model/" + directory
+
+        # Make path if not yet exists
+        if not os.path.exists(path):
+            os.mkdir(path)
+
+        avg_frames_per_minute = total_frames / (total_duration / 60)
+        # avg_episodes_per_minute = total_episodes / (total_duration / 60)
+        avg_original_reward_per_episode = np.mean(total_original_rewards[-50:])
+        avg_shaped_reward_per_episode = np.mean(total_shaped_rewards[-50:])
+
+        line = ("{: 5d}".format(total_episodes)
+                + " {: 5d}".format(episode_frames) + "f"
+                + " {: 4d}".format(round(episode_duration)) + "s"
+                + " {: 4d}".format(round(avg_frames_per_minute)) + "f/min"
+                + "     "
+                + " reward {: 5f}".format(round(episode_original_reward, 2))
+                + " reward(shaped) {: 5f}".format(round(episode_shaped_reward, 2))
+                + " avg reward per episode {: 3f}".format(round(avg_original_reward_per_episode, 2))
+                + " avg reward(shaped) per episode {: 3f}".format(round(avg_shaped_reward_per_episode, 2))
+                + " loss " + str(round(episode_loss, 4)))
+
+        # Print log
+        print(line)
+
+        # Write log into file
+        log_file = open(path + "/log.txt", "a")
+        log_file.write(line)
+        log_file.close()
