6cabd3478d467267485fab331b9e674be4ceba68
Florian Schwanz <florian.schwanz@interoberlin.de>
2020-06-16 08:12:03 +0200

    Fix model optimizer

--- notebooks/00-basemodel/atari-dqn/lib/model_optimizer.py
+++ notebooks/00-basemodel/atari-dqn/lib/model_optimizer.py
@@ -7,7 +7,7 @@ import torch.nn.functional as F
 # Training loop
 # ^^^^^^^^^^^^^
 #
-# Finally, the code for training our output.
+# Finally, the code for training our model.
 #
 # Here, you can find an ``optimize_model`` function that performs a
 # single step of the optimization. It first samples a batch, concatenates
@@ -29,7 +29,7 @@ class ModelOptimizer:
 
     def optimize_model(policy_net, target_net, optimizer, memory, batch_size, gamma, device):
         """
-        Optimizes output
+        Optimizes model
         :param policy_net: policy net
         :param target_net: target net
         :param optimizer: optimizer
@@ -52,23 +52,28 @@ class ModelOptimizer:
         # to Transition of batch-arrays.
         batch = Transition(*zip(*transitions))
 
+        # Compute a mask of non-final states and concatenate the batch elements
+        # (a final state would've been the one after which simulation ended)
+        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                                batch.next_state)), device=device, dtype=torch.bool)
+        non_final_next_states = torch.cat([s for s in batch.next_state
+                                           if s is not None])
+
         state = torch.cat(batch.state)
         action = torch.cat(batch.action)
         next_state = torch.cat(batch.next_state)
         reward = torch.cat(batch.reward)
 
         # Compute Q(s_t, a)
-        q_values = policy_net(state)
+        q_values = policy_net(state).gather(1, action)
         # Compute V(s_{t+1}) for all next states.
-        next_q_values = policy_net(next_state)
-        next_q_state_values = target_net(next_state)
+        next_q_values = torch.zeros(batch_size, device=device)
+        next_q_state_values = target_net(non_final_next_states).max(1)[0].detach()
         # Compute the expected Q values
-        q_value = q_values.gather(1, action.unsqueeze(1)).squeeze(1)
-        next_q_value = next_q_state_values.gather(1, torch.max(next_q_values, 1)[1].unsqueeze(1)).squeeze(1)
-        expected_q_value = reward + gamma * next_q_value * (1 - done)
+        expected_q_values = (next_q_values * gamma) + reward
 
         # Compute Huber loss
-        loss = F.smooth_l1_loss(q_value, expected_q_value.unsqueeze(1))
+        loss = F.smooth_l1_loss(q_values, expected_q_values.unsqueeze(1))
 
         # Optimize the output
         optimizer.zero_grad()
